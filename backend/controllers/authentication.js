48d7af927e2738d70e18df04dd966ab91a967b27
Benji250 <123209910+Benji250@users.noreply.github.com>
2023-09-11 16:54:22 -0700

    worked on hw day 4 left off at step 4

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('jwt')
 
 const { User } = db
 
@@ -17,11 +18,28 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userid })
+        // req.session.userid = user.userid
+        res.json({ user: user, token: result.value })
     }
 })
 
-  
+___
+___
+router.get('/profile', async (req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userid
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
+
+
 
 
 module.exports = router
